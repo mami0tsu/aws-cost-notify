928af214c2aae38d30bc1a0369dcab8652ead85c
mami0tsu <mami0tsu.jp@gmail.com>
2023-03-04 21:50:47 +0900

    add terraform backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,17 @@
 terraform {
   required_version = "1.3.9"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.56.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "mami0tsu-tfstate"
+    key = "aws-cost-notify/terraform.tfstate"
+    region = "ap-northeast-1"
   }
 }
 
